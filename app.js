d9a6e1f1eb8f9587563d3a0c5294c449aae592fc
samade747 <samad.e747@gmail.com>
2023-10-09 15:06:53 +0500

    assa

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-function bulbOn() {
-    document.getElementById('img').src = "./images/on.jpeg"
+let isBulbOn = false;
 
+function toggle(){
+    const imgElement = document.getElementById('img');
+    const imgElement1 = document.getElementById('bb');
+
+    if (isBulbOn){
+        imgElement.src = "./images/off.jpeg";
+    } else {
+        imgElement.src = "./images/on.jpeg";
+        imgElement.style.backgroundColor = 'yellow';
+    }
+isBulbOn = !isBulbOn;
 
 }
+
+
+
+
+
+
+
+// function bulbOn() {
+//     document.getElementById('img').src = "./images/on.jpeg"
+    
+
+// }
+
+// function bulbOff(){
+//     document.getElementById('img').src = "./images/off.jpeg"
+    
+// }
